7ffe3e51ac4043fcbdea91ab97f7a89963bb630d
cestpasphoto <cestpasphoto@gmail.com>
2022-11-08 07:10:04 +0100

    Replace character by nicer SVG

--- santorini/main.js
+++ santorini/main.js
@@ -6,9 +6,9 @@
 // Check also constants_*.js
 
 const directions_char = ['↖', '↑', '↗', '←', 'Ø', '→', '↙', '↓', '↘'];
-const levels_array = ['‌', '▂', '▅', '█', 'X'];
-const worker_string = '웃';
-const no_worker_string = '‌';
+
+const green = '#21BA45';
+const red   = '#DB2828';
 
 /* =================== */
 /* =====  ONNX   ===== */
@@ -49,6 +49,43 @@ function moveToString(move, subject='One') {
   description += (power > 0) ? (' using power of god' + power) : '';
   description += ' (move ' + move + ')';
   return description;
+}
+
+function generateSvg(nb_levels, worker) {
+  const width = 240, height = 240, level_height = 40, level_shrink_x = 30;
+  let style = 'style=""';
+  if (worker > 0) {
+    style = `style="fill: ${green};"`;
+  } else if (worker < 0) {
+    style = `style="fill: ${red};"`;
+  }
+
+  let svg = `<svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}" xml:space="preserve">`;
+
+  // Draw base rectangles
+  let x_beg = 0, x_end = 0, y_beg = height, y_end = height;
+  for (let l=0; l<nb_levels && l<3; l++) {
+    x_beg = level_shrink_x * l, x_end = width - level_shrink_x * l;
+    y_beg = height - level_height * l, y_end = height - level_height * (l+1);
+    svg += `<polygon ${style} points="${x_beg},${y_beg} ${x_beg},${y_end} ${x_end},${y_end} ${x_end},${y_beg}"/>`;
+  }
+
+  // Draw upper item: either dome, either worker, either nothing
+  if (nb_levels == 4) {
+    svg += `<path d="M 70 0 A 50 50, 0, 0 1, 170 0" style="fill: blue;" transform="translate(0 ${3*level_height})"/>`;
+  } else if (worker != 0) {
+    svg += `<g ${style} transform="translate(70 ${height - nb_levels*level_height - 100})">`;
+    if (Math.abs(worker) == 1) {
+      svg += '<path d="M66.403,29.362C68.181,18.711,60.798,10,50,10l0,0c-10.794,0-18.177,8.711-16.403,19.362l2.686,16.133 c1.068,6.393,7.24,11.621,13.718,11.621l0,0c6.481,0,12.649-5.229,13.714-11.621L66.403,29.362z"/>';
+      svg += '<path d="M64.007,58.001c-3.76,3.535-8.736,5.781-14.007,5.781s-10.247-2.246-14.007-5.781l-19.668,6.557 C12.845,65.716,10,69.668,10,73.333V90h80V73.333c0-3.665-2.845-7.617-6.325-8.775L64.007,58.001z"/>';
+    } else {
+      svg += '<path d="M83.675,64.558l-19.668-6.557c-3.76,3.535-8.736,5.781-14.007,5.781s-10.247-2.246-14.007-5.781l-19.668,6.557 C12.845,65.716,10,69.668,10,73.333V90h80V73.333C90,69.668,87.155,65.716,83.675,64.558z"/>';
+      svg += '<path d="M76.328,50c-6.442-6.439-9.661-14.886-9.661-23.333h-0.029C66.862,17.282,59.87,10,50,10 c-9.863,0-16.855,7.282-16.638,16.667h-0.029c0,8.447-3.219,16.895-9.661,23.333l4.714,4.717c3.189-3.189,5.726-6.846,7.637-10.791 l0.26,1.569c1.068,6.394,7.24,11.622,13.718,11.622c6.481,0,12.649-5.228,13.714-11.622l0.264-1.572 c1.911,3.945,4.447,7.605,7.637,10.794L76.328,50z"/>';
+    }
+    svg += `</g>`;
+  }
+  svg += '</svg>';
+  return svg;
 }
 
 /* =================== */
@@ -444,14 +481,8 @@ function refreshBoard() {
       let worker = game.board[y][x][0];
       let selectable = move_sel.isSelectable(y, x);
 
-      // set cell content and color
-      if (worker > 0) {
-        cell_content = '<span class="ui green text">' + worker_string + '<br>' + levels_array[level] + '</span>';
-      } else if (worker < 0) {
-        cell_content = '<span class="ui red text">'   + worker_string + '<br>' + levels_array[level] + '</span>';
-      } else {
-        cell_content = '<span class="ui text">'    + no_worker_string + '<br>' + levels_array[level] + '</span>';
-      }
+      // generateSvg deals with nb of levels, dome, worker and color
+      cell_content = '<div class="ui middle aligned tiny image">' + generateSvg(level, worker) + '</div>';
 
       // set cell background and ability to be clicked
       if (editMode > 0) {
